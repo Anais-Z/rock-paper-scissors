144bea490e4b69b59f7762704293f522be346e86
Anais <anaiszed447@gmail.com>
2022-08-24 04:51:39 -0400

    Created playerChoice and player helper functions

--- script.js
+++ script.js
@@ -5,6 +5,51 @@ function getComputerChoice(){
     //choose random value from array
     let singleChoice = choices[Math.floor(Math.random() * choices.length)]
 
-    return singleChoice
+    return console.log(`Computer chose ${singleChoice}`)
+}
 
+function playRound(playerChoice, computerChoice){
+    //checks which item the user chose
+    switch(playerChoice){
+        case "rock":
+            return playerRock(computerChoice)
+        case "paper":
+            return playerPaper(computerChoice)
+        case "scissors":
+            playerScissors(computerChoice)
+    }
+
+}
+
+//evaluates if player chose rock
+function playerRock(computerChoice){
+    if(computerChoice === "scissors"){
+        return console.log("You Win")
+    }else if(computerChoice === "paper"){
+        return console.log("You Lose")
+    }else{
+        return console.log("It's a TIe")
+    }
+}
+
+//evaluates if player chose paper
+function playerPaper(computerChoice){
+    if(computerChoice === "rock"){
+        return console.log("You Win")
+    }else if(computerChoice === "scissors"){
+        return console.log("You Lose")
+    }else{
+        return console.log("It's a TIe")
+    }
+}
+
+//evaluates if player chose scissors
+function playerScissors(computerChoice){
+    if(computerChoice === "paper"){
+        return console.log("You Win")
+    }else if(computerChoice === "rock"){
+        return console.log("You Lose")
+    }else{
+        return console.log("It's a TIe")
+    }
 }
