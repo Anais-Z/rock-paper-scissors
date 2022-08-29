21153818e9f1556f523c6e97e89fcddc9cbebd03
Anais <anaiszed447@gmail.com>
2022-08-29 18:04:08 -0400

    Re-typed the logic for the game

--- script.js
+++ script.js
@@ -1,77 +1,129 @@
 function getComputerChoice(){
     //3 choices stored in an array
-    let choices = ["rock", "paper", "scissors"]
+    let choices = ["rock", "paper", "scissors"];
 
     //choose random value from array
-    let singleChoice = choices[Math.floor(Math.random() * choices.length)]
-    console.log(`Computer chose ${singleChoice}`)
-    return singleChoice
+    let singleChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    return singleChoice;
 }
 
 function playRound(playerChoice, computerChoice){
+    let roundWinner = null;
     //checks which item the user chose
     switch(playerChoice){
         case "rock":
-            return playerRock(computerChoice)
+           return roundWinner = playerRock(computerChoice);
         case "paper":
-            return playerPaper(computerChoice)
+          return  roundWinner = playerPaper(computerChoice);
         case "scissors":
-            playerScissors(computerChoice)
+           return roundWinner = playerScissors(computerChoice);
     }
+
+    
 
 }
 
 //evaluates if player chose rock
 function playerRock(computerChoice){
     if(computerChoice === "scissors"){
-        return alert("You Win")
+        return "Player Wins";
     }else if(computerChoice === "paper"){
-        return alert("You Lose")
+        return "Computer Wins";
     }else{
-        return alert("It's a TIe")
+        return "Tie";
     }
 }
 
 //evaluates if player chose paper
 function playerPaper(computerChoice){
     if(computerChoice === "rock"){
-        return alert("You Win")
+        return "Player Wins";
     }else if(computerChoice === "scissors"){
-        return alert("You Lose")
+        return "Computer Wins";
     }else{
-        return alert("It's a TIe")
+        return "Tie";
     }
 }
 
 //evaluates if player chose scissors
 function playerScissors(computerChoice){
     if(computerChoice === "paper"){
-        return alert("You Win")
+        return "Player Wins";
     }else if(computerChoice === "rock"){
-        return alert("You Lose")
+        return "Computer Wins";
     }else{
-        return alert("It's a TIe")
+        return "Tie";
     }
 }
 
-function game(e){
+function winner(playerScore, compScore){
+    
+    if(playerScore === 5){
+        result.textContent = "Player Wins"
+        return "Finish"
 
- //   for(let i = 0; i < 5; i++){
-        // const playerSelection = prompt("Choose rock, paper or scissors", "rock")
+    }else if (compScore === 5){
+        result.textContent = "Computer Wins"
+        return "Finish"
+    }
 
-        // console.log(`You chose ${playerSelection}`)
-        // const computerSelection = getComputerChoice()
-        
-        // playRound(playerSelection.toLowerCase(),computerSelection)
-
-  //  }
-
-  console.log(e.target.value)
+    
+    
 }
 
+function createReplayButton(){
+    groupOfButtons.appendChild(replayButton)
+    replayButton.addEventListener('click', restartProgram)
+}
 
-const buttons = document.querySelectorAll('button')
+function restartProgram(){
+    window.location.reload()
+}
 
-buttons.forEach((button) => {
-    button.addEventListener('click', game)
-})
+function game(e){
+    let playerSelection = e.target.value;
+    const computerSelection = getComputerChoice()  ;
+    let current = playRound(playerSelection ,computerSelection);
+    console.log(current);
+
+    if(current == 'Player Wins'){
+        currentPlayerScore++;
+        console.log(`Player score = ${currentPlayerScore}`);
+        playerScore.textContent = currentPlayerScore;
+    } else if(current == "Computer Wins"){
+        currentCompScore++;
+       compScore.textContent = currentCompScore;
+        console.log(`Comp score = ${currentCompScore}`);
+    };
+
+   let status =  winner(currentPlayerScore, currentCompScore)
+
+   if(status == "Finish"){
+    buttons.forEach(button => button.disabled = true)
+    createReplayButton()
+   }
+    
+};
+
+
+
+const playerScore = document.querySelector("#player-score");
+const compScore = document.querySelector("#comp-score");
+
+const result = document.querySelector('.result');
+
+let currentPlayerScore = 0;
+let currentCompScore = 0;
+let gameStatus = ""
+
+const buttons = document.querySelectorAll('button');
+
+const groupOfButtons = document.querySelector('.the-buttons')
+
+buttons.forEach((button) => {;
+    button.addEventListener('click', game);
+});
+
+const replayButton = document.createElement('button')
+replayButton.innerText = "Play Again"
